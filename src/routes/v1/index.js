dfb7c7300b054190cd394aca914861996f0242f4
Varun Maheshwari <maheshwarivarun82@gmail.com>
2024-01-01 21:58:43 +0530

    "added user isAuthentication function"

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -14,4 +14,11 @@ router.post(
     AuthRequestValidator.validateUserAuth,
     UserController.signIn);
 
+
+router.get(
+    '/isAuthenticated',
+    UserController.isAuthenticated
+)
+
+
 module.exports=router
