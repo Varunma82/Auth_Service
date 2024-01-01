56a99763b082276879c372894118d40f4c451158
Varun Maheshwari <maheshwarivarun82@gmail.com>
2024-01-01 15:56:16 +0530

    First commit: project skeloton setup done

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express=require('express')
+const {PORT}=require('./config/serverConfig')
+
+const app=express();
+
+
+const prepareAndStartServer=()=>{
+
+    app.listen(PORT, () => {
+        console.log(`Server started on PORT : ${PORT}`);
+    });
+}
+
+prepareAndStartServer();
